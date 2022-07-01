b1ac686ea14a8bd563465f7fda294bbf7a8cf911
Jackfr0stt <57899488+Jackfr0stt@users.noreply.github.com>
2022-07-01 18:15:06 +0100

    Formats file

--- src/__tests__/acceptance/Test2.test.js
+++ src/__tests__/acceptance/Test2.test.js
@@ -1,14 +1,14 @@
 const testlab = require('@loopback/testlab');
-    const axios = require('axios');
+const axios = require('axios');
 describe('List all APIs tests', async () => {
-      describe('Test group 1 tests', async () => {
-        it('Test 2', async () => {
-          const res = await axios.get("http://localhost:8080/apis");
-testlab.expect(res.data[0]).to.hasOwnProperty("id");
-testlab.expect(res.data[0]).to.hasOwnProperty("apiname");
-testlab.expect(res.data[0]).to.hasOwnProperty("apitype");
-testlab.expect(res.data[0]).to.hasOwnProperty("domain");
-testlab.expect(res.data[0]).to.hasOwnProperty("userid");
-});
-});
+  describe('Test group 1 tests', async () => {
+    it('Test 2', async () => {
+      const res = await axios.get("http://localhost:8080/apis");
+      testlab.expect(res.data[0]).to.hasOwnProperty("id");
+      testlab.expect(res.data[0]).to.hasOwnProperty("apiname");
+      testlab.expect(res.data[0]).to.hasOwnProperty("apitype");
+      testlab.expect(res.data[0]).to.hasOwnProperty("domain");
+      testlab.expect(res.data[0]).to.hasOwnProperty("userid");
+    });
+  });
 });
